09b053149123679cdbf47fe5558f5e5b67eb9a84
Chris Sheard <csheard@uvic.ca>
2024-07-07 17:42:32 -0600

    Adding default created MAUI app

---
+++ HarleyFeedingTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HarleyFeedingTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HarleyFeedingTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HarleyFeedingTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
